6b9e3c827313054bbcbd01f5b4aedbcea9072829
Diucla <claudiosingular@gmail.com>
2017-09-10 15:24:09 +0200

    Resolvendo bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FeedComponent } from './feed/feed.component';
-<<<<<<< HEAD
 import { TweetComponent } from './tweet/tweet.component';
-=======
-import { LinksCssComponent } from './links-css/links-css.component';
->>>>>>> parent of 9d3ae49... Component tweet
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FeedComponent,
-<<<<<<< HEAD
-    TweetComponent
-=======
-    LinksCssComponent
->>>>>>> parent of 9d3ae49... Component tweet
+    TweetComponent    
   ],
   imports: [
     BrowserModule
