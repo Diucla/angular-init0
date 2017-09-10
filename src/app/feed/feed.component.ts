bd5a3e92d043847a0a4a448b4a87337c33291754
Diucla <claudiosingular@gmail.com>
2017-09-10 10:17:28 +0200

    Adicao de Menu e Feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+    teste = [
+        {user: "Sumbus"}
+    ];
+
+  tweets  = [
+
+      {
+      user: "Stevie",
+      name: "Stevie Feliciano",
+      body: "is a library scientist living " +
+      "in New York City. She likes to spend her time reading, " +
+      "running, and writing.",
+      avatar: "../../assets/avatar/stevie.jpg"
+    },
+
+      {
+          user: "Veronika",
+          name: "Veronika Ossi",
+          body: "is a set designer living in New York who enjoys kittens, music, and partying.",
+          avatar: "../../assets/avatar/veronika.jpg"
+      },
+
+      {
+          user: "Jenny",
+          name: "Jenny Hess",
+          body: " is a student studying Media Management at the New School.",
+          avatar: "../../assets/avatar/jenny.jpg"
+      }
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
