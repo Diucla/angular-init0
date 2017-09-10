e651f717847303c87a61736d8bc5a48f434db597
Diucla <claudiosingular@gmail.com>
2017-09-10 08:16:42 +0200

    Adicao de cdns da semantica-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.html', '../assets/css/materialize.min.css']
+  styleUrls: ['./app.component.html']
 
 })
 export class AppComponent {
